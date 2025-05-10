e52dad6b8557b89f7cd298d345a8d46ba1e070da
Ihor Demchenko <demchenkoigor24@gmail.com>
2025-05-10 16:29:45 +0300

    PA1 Add torus, WebCam background, control panel

--- Model.js
+++ Model.js
@@ -1,120 +1,118 @@
-
-
-function deg2rad(angle) {
-    return angle * Math.PI / 180;
-}
-
-
-function Vertex(p)
-{
-    this.p = p;
-    this.normal = [];
-    this.triangles = [];
-}
-
-function Triangle(v0, v1, v2)
-{
-    this.v0 = v0;
-    this.v1 = v1;
-    this.v2 = v2;
-    this.normal = [];
-    this.tangent = [];
-}
-
-// Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
     this.iIndexBuffer = gl.createBuffer();
     this.count = 0;
+    this.type = gl.TRIANGLES;
 
-    this.BufferData = function(vertices, indices) {
-
+    this.BufferData = function (vertices, indices) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STREAM_DRAW);
-
-        this.count = indices.length;
+        if (indices) {
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STREAM_DRAW);
+            this.count = indices.length;
+        } else {
+            this.count = vertices.length / 3;
+        }
     }
 
-    this.Draw = function() {
+    this.Draw = function () {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-        //gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-        gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
+        if (this.type === gl.TRIANGLES && this.iIndexBuffer) {
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+            gl.drawElements(this.type, this.count, gl.UNSIGNED_SHORT, 0);
+        } else {
+            gl.drawArrays(this.type, 0, this.count);
+        }
     }
 
-    this.DrawWireframe = function() {
-
-        for (let p=0; p<this.count; p+=3)                    // offset in bytes (UNSIGNED_SHORT is two bytes)
-            gl.drawElements(gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, p*2);
-    }
-}
-
-
-function CreateSurfaceData(data)
-{
-    let vertices = [];
-    let triangles = [];
-
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 0, Math.cos(deg2rad(ang))] ));
+    this.DrawWireframe = function () {
+        if (this.iIndexBuffer) {
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+            for (let p = 0; p < this.count; p += 3)
+                gl.drawElements(gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, p * 2);
+        }
     }
 
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
+    // Surface of Revolution of a General Sinusoid
+    this.CreateVertex = function (a, n, R, r, b) {
+        const x = r * Math.cos(b),
+            y = r * Math.sin(b),
+            z = a * Math.cos(n * Math.PI * r / R);
+        return [x, y, z];
+    }
 
-        let v0ind = vertices.length;
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 1, Math.cos(deg2rad(ang))] ));
+    this.CreateAstroidalTorus = function (a, r, segmentsCount, theta) {
+        let vertices = [];
+        let indices = [];
 
-        // v0    v2 
-        //   o - o
-        //   | \ |
-        //   o - o
-        // v3     v1
+        let uSegments = segmentsCount;
+        let vSegments = segmentsCount;
 
-        if (i > 0)
-        {
-            let v1ind = v0ind - 72 -1;
-            let v2ind = v0ind - 1;
-            let v3ind = v0ind - 72
+        let vertexCount = (uSegments + 1) * (vSegments + 1);
 
-            let trian = new Triangle(v0ind, v1ind, v2ind);
-            let trianInd = triangles.length;
+        for (let i = 0; i <= uSegments; i++) {
+            let u = -Math.PI + (2 * Math.PI * i) / uSegments;
+            if (i === uSegments) u = -Math.PI;
 
-            triangles.push( trian );
-            vertices[v0ind].triangles.push(trianInd);
-            vertices[v1ind].triangles.push(trianInd);
-            vertices[v2ind].triangles.push(trianInd);
+            let x_u = a * Math.pow(Math.cos(u), 3);
+            let z_u = a * Math.pow(Math.sin(u), 3);
 
-            let trian2 = new Triangle(v0ind, v3ind, v1ind);
-            let trianInd2 = triangles.length;
+            for (let j = 0; j <= vSegments; j++) {
+                let v = (2 * Math.PI * j) / vSegments;
+                if (j === vSegments) v = 0;
 
-            triangles.push( trian2 );
-            vertices[v0ind].triangles.push(trianInd2);
-            vertices[v3ind].triangles.push(trianInd2);
-            vertices[v1ind].triangles.push(trianInd2);
+                let X = (r + x_u * Math.cos(theta) - z_u * Math.sin(theta)) * Math.cos(v);
+                let Y = (r + x_u * Math.cos(theta) - z_u * Math.sin(theta)) * Math.sin(v);
+                let Z = x_u * Math.sin(theta) + z_u * Math.cos(theta);
 
+                vertices.push(X, Y, Z);
+            }
         }
 
-    }
+        for (let i = 0; i < uSegments; i++) {
+            for (let j = 0; j < vSegments; j++) {
+                let current = i * (vSegments + 1) + j;
+                let next = (i + 1) * (vSegments + 1) + j;
 
-    data.verticesF32 = new Float32Array(vertices.length*3);
-    for (let i=0, len=vertices.length; i<len; i++)
-    {
-        data.verticesF32[i*3 + 0] = vertices[i].p[0];
-        data.verticesF32[i*3 + 1] = vertices[i].p[1];
-        data.verticesF32[i*3 + 2] = vertices[i].p[2];
-    }
+                indices.push(current, next, current + 1);
+                indices.push(current + 1, next, next + 1);
+            }
+        }
 
-    data.indicesU16 = new Uint16Array(triangles.length*3);
-    for (let i=0, len=triangles.length; i<len; i++)
-    {
-        data.indicesU16[i*3 + 0] = triangles[i].v0;
-        data.indicesU16[i*3 + 1] = triangles[i].v1;
-        data.indicesU16[i*3 + 2] = triangles[i].v2;
+        // u-seam
+        for (let j = 0; j < vSegments; j++) {
+            let current = (uSegments - 1) * (vSegments + 1) + j;
+            let next = j;
+
+            indices.push(current, next, current + 1);
+            indices.push(current + 1, next, next + 1);
+        }
+
+        // v-seam
+        for (let i = 0; i < uSegments; i++) {
+            let current = i * (vSegments + 1) + (vSegments - 1);
+            let next = (i + 1) * (vSegments + 1) + (vSegments - 1);
+
+            indices.push(current, next, i * (vSegments + 1));
+            indices.push(i * (vSegments + 1), next, (i + 1) * (vSegments + 1));
+        }
+
+        // кут
+        let last = (uSegments - 1) * (vSegments + 1) + (vSegments - 1);
+        let first = 0;
+
+        indices.push(last, vSegments - 1, uSegments * (vSegments + 1) - 1);
+        indices.push(uSegments * (vSegments + 1) - 1, vSegments - 1, first);
+
+        this.BufferData(new Float32Array(vertices), new Uint16Array(indices));
     }
 
 }
